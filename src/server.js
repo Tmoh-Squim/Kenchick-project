c4fc1d653b3dfcd61f3df1408accd256263b2eda
squim <squim@localhost.localdomain>
2024-06-02 18:28:26 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -1,2 +1,4 @@
-export const Server_Url = "http://localhost:8080/api/v1"
-export const Server = "http://localhost:8080"
+export const server_Url = "http://localhost:8080/api/v1"
+export const server = "http://localhost:8080"
+export const Server_Url = 'https://kenchick.onrender.com/api/v1'
+export const Server = 'https://kenchick.onrender.com'
