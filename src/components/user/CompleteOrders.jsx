9931683a763a0f44b00f03bf3c205294e3e035ed
squim <squim@localhost.localdomain>
2024-06-04 11:24:30 +0300

    updated

--- src/components/user/CompleteOrders.jsx
+++ src/components/user/CompleteOrders.jsx
@@ -1,74 +1,72 @@
-import { Table } from 'antd';
-import { Content } from 'antd/es/layout/layout';
-import React, { useEffect, useState } from 'react';
-import { AiOutlineEye } from 'react-icons/ai';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Table } from 'antd'
+import { Content } from 'antd/es/layout/layout'
+import React, { useEffect, useState } from 'react'
+import { AiOutlineEye } from 'react-icons/ai'
+import { useSelector } from 'react-redux'
+import {useNavigate} from "react-router-dom"
 
 const CompletedOrders = () => {
-  const { orders } = useSelector((state) => state.adminOrders.orders);
-  const [data, setData] = useState([]);
+  const {orders} = useSelector((state)=>state.adminOrders.orders);
+  const [data,setData] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const dat = orders.filter((order) => order.status === 'Delivered');
-    setData(dat);
-  }, [orders]);
+  useEffect(()=>{  
+    const dat = orders.filter((order)=>order.status === 'Delivered');
+    setData(dat)
+  },[])
+  
 
   return (
-    <Content>
-      <Table
-        dataSource={data}
-        columns={[
-          {
-            title: "Id",
-            key: "_id",
-            dataIndex: "_id",
-            responsive: ['md']
-          },
-          {
-            title: "Item Qty",
-            key: "title",
-            render: (text, record) => record.cart.length,
-            responsive: ['md']
-          },
-          {
-            title: "Total",
-            key: "total",
-            dataIndex: "totalPrice",
-            responsive: ['md']
-          },
-          {
-            title: "Created At",
-            key: "createdAt",
-            dataIndex: 'createdAt',
-            responsive: ['md']
-          },
-          {
-            title: "Status",
-            key: "status",
-            dataIndex: 'status',
-            responsive: ['md']
-          },
-          {
-            title: "Preview",
-            key: "Preview",
-            dataIndex: "_id",
-            className: 'cursor-pointer',
-            render: (id, order) => (
-              <AiOutlineEye size={28}
-                onClick={() => {
-                  navigate(`/update-order/${id}`, { state: { order: order } });
-                }}
-              />
-            ),
-            responsive: ['md']
-          },
-        ]}
-        scroll={{ x: 'max-content' }}
-      />
-    </Content>
-  );
+   <Content>
+    <Table
+    dataSource={data}
+    columns={[
+      {
+        title:"Id",
+        key:"_id",
+        dataIndex:"_id"
+      },
+
+      {
+        title:"Item Qty",
+        key:"title",
+        render: (text, record) => record.cart.length,
+      },
+      {
+        title:"Total",
+        key:"total",
+        dataIndex:"totalPrice"
+      },
+      {
+        title:"Created At",
+        key:"createdAt",
+        dataIndex:'createdAt'
+      },
+      {
+        title:"Status",
+        key:"status",
+        dataIndex:'status'
+      },
+      {
+        title:"Preview",
+        key:"Preview",
+        dataIndex:"_id",
+        className:'cursor-pointer',
+        render:(id,order)=>(
+          
+          <AiOutlineEye size={28}
+          onClick={()=>{
+            navigate(`/update-order/${id}`,{state:{order: order}})
+          }}
+           />
+        )
+      },
+
+    ]}
+    />
+
+   </Content>
+  )
 }
 
-export default CompletedOrders;
+export default CompletedOrders
